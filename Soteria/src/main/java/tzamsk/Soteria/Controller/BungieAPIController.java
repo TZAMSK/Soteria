70e12ad60a88e2df283b27a43c03ac71b955f7f6
Alikhan Khakimov <akhakimov@dept-info.crosemont.quebec>
2024-07-06 23:55:01 -0400

    Played around Bungie's Manifest tried to pull the secondarySpecial emblem as background

--- Soteria/src/main/java/tzamsk/Soteria/Controller/BungieAPIController.java
+++ Soteria/src/main/java/tzamsk/Soteria/Controller/BungieAPIController.java
@@ -34,6 +34,7 @@ public class BungieAPIController {
 
         String url = String.format("%s/%s/Profile/%s/?components=%d", rootURL, membershipType, membershipId, 100);
         String charactersUrl = String.format("%s/%s/Profile/%s/?components=%d", rootURL, membershipType, membershipId, 200);
+        String triumphsUrl = String.format("%s/%s/Profile/%s/?components=%d", rootURL, membershipType, membershipId, 900);
 
         HttpHeaders headers = new HttpHeaders();
         headers.set("X-API-Key", apiKey);
@@ -42,6 +43,7 @@ public class BungieAPIController {
 
         ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
         ResponseEntity<String> charactersResponse = restTemplate.exchange(charactersUrl, HttpMethod.GET, entity, String.class);
+        ResponseEntity<String> triumphsResponse = restTemplate.exchange(triumphsUrl, HttpMethod.GET, entity, String.class);
 
         ObjectMapper mapper = new ObjectMapper();
 
@@ -51,11 +53,16 @@ public class BungieAPIController {
         int renewedGuardianRank = 0;
         int bungieGlobalDisplayNameCode = 0;
 
+        int currentTriumphScore = 0;
+        int legacyTriumphScore = 0;
+        int totalTriumphScore = 0;
+
         List<JsonNode> characters = new ArrayList<>();
 
         try {
             JsonNode root = mapper.readTree(response.getBody());
             JsonNode charactersRoot = mapper.readTree(charactersResponse.getBody());
+            JsonNode triumphsRoot = mapper.readTree(triumphsResponse.getBody());
 
             JsonNode charactersData = charactersRoot.path("Response").path("characters").path("data");
             charactersData.forEach(character -> {
@@ -67,6 +74,11 @@ public class BungieAPIController {
             currentGuardianRank = root.path("Response").path("profile").path("data").path("currentGuardianRank").asInt();
             lifetimeHighestGuardianRank = root.path("Response").path("profile").path("data").path("lifetimeHighestGuardianRank").asInt();
             renewedGuardianRank = root.path("Response").path("profile").path("data").path("renewedGuardianRank").asInt();
+
+            JsonNode profileRecordsData = triumphsRoot.path("Response").path("profileRecords").path("data");
+            currentTriumphScore = profileRecordsData.path("activeScore").asInt();
+            legacyTriumphScore = profileRecordsData.path("legacyScore").asInt();
+            totalTriumphScore = profileRecordsData.path("lifetimeScore").asInt();
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -82,31 +94,52 @@ public class BungieAPIController {
         mav.addObject("lifetimeHighestGuardianRank", lifetimeHighestGuardianRank);
         mav.addObject("renewedGuardianRank", renewedGuardianRank);
         mav.addObject("characters", characters);
+
+        mav.addObject("currentTriumphScore", currentTriumphScore);
+        mav.addObject("legacyTriumphScore", legacyTriumphScore);
+        mav.addObject("totalTriumphScore", totalTriumphScore);
         return mav;
     }
 
     @GetMapping("/profile/{membershipType}/{membershipId}/{characterId}")
     @ResponseBody
-    public ModelAndView getUserProfile(@PathVariable String membershipType,
-                                       @PathVariable String membershipId,
-                                       @PathVariable String characterId){
+    public ModelAndView getUserCharacterProfile(@PathVariable String membershipType,
+                                                @PathVariable String membershipId,
+                                                @PathVariable String characterId){
 
         String characterUrl = String.format("%s/%s/Profile/%s/Character/%s/?components=%d", rootURL, membershipType, membershipId, characterId, 900);
+        String emblemUrl = String.format("%s/%s/Profile/%s/Character/%s/?components=%d", rootURL, membershipType, membershipId, characterId, 200);
+
         HttpHeaders headers = new HttpHeaders();
         headers.set("X-API-Key", apiKey);
         HttpEntity<String> entity = new HttpEntity<>(headers);
 
         ResponseEntity<String> characterResponse = restTemplate.exchange(characterUrl, HttpMethod.GET, entity, String.class);
+        ResponseEntity<String> emblemResponse = restTemplate.exchange(emblemUrl, HttpMethod.GET, entity, String.class);
 
-        ObjectMapper mapper = new ObjectMapper();;
+        ObjectMapper mapper = new ObjectMapper();
+
+        String emblemBackgroundPath = "";
         try {
-            JsonNode characterRoot = mapper.readTree(characterResponse.getBody());
+            JsonNode characterData = mapper.readTree(emblemResponse.getBody()).path("Response").path("characters").path("data").path(characterId);
+            JsonNode emblemNode = characterData.path("emblemHash");
+
+            if (!emblemNode.isMissingNode()) {
+                String emblemHash = emblemNode.asText();
+                String emblemDetailsUrl = String.format("https://www.bungie.net/Platform/Destiny2/Manifest/DestinyInventoryItemDefinition/%s/", emblemHash);
+
+                ResponseEntity<String> emblemDetailsResponse = restTemplate.exchange(emblemDetailsUrl, HttpMethod.GET, entity, String.class);
+                JsonNode emblemDetails = mapper.readTree(emblemDetailsResponse.getBody()).path("Response");
+
+                emblemBackgroundPath = emblemDetails.path("secondarySpecial").asText();
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
 
         ModelAndView mav = new ModelAndView("profile-character");
         mav.addObject("characterStats", characterResponse.getBody());
+        mav.addObject("emblemBackgroundPath", emblemBackgroundPath);
         return mav;
     }
 }
